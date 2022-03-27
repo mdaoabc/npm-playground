144b349cf71abfb7d917ff6a3e942dbb6cf013ab
Mike Dao <mdao@atlassian.com>
2022-03-28 10:33:34 +1100

    NOISSUE: Updated console log.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const { getPenguins } = require('mdao_dep_package');
 
 console.log(getPenguins({ penguins: true }));
 
-console.log('I am adding a very important console log');
+console.log('I am adding a very important console log.');
